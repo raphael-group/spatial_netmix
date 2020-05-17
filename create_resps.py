6bd6776f939a4351bb71843cd3416cf166ef90b8
Kimberly Ding <dingkimberly@gmail.com>
2020-05-17 18:14:41 -0400

    Hm

--- create_resps.py
+++ create_resps.py
@@ -4,7 +4,7 @@ import numpy as np
 from KSS_EM import em
 
 ####################################################################
-# HELPER FUNCTION
+# HELPER FUNCTIONS
 ####################################################################
 
 # shift responsibilities so alpha_est*n of them are positive
@@ -15,15 +15,44 @@ def shift_resps(resps, alpha_est):
     threshold = 0.5*(sorted_resps[max(0, k-1)] + sorted_resps[min(k, n-1)])
     return resps - threshold
 
+# converts column col of some tsv list (no headers) to numpy float array
+def tsv_to_np(filename, col=0):
+    
+    res = []
+    
+    with open(filename, 'r') as fp:
+        reader = csv.reader(fp, delimiter='\t')
+    
+        for row in reader:
+            if len(row[col]) > 0:
+                res.append(float(row[col]))
+            
+    return np.array(res)
+
+def tsv_to_array(filename, col=0):
+    res = []
+    with open(filename, 'r') as fp:
+        reader = csv.reader(fp, delimiter='\t')
+    
+        for row in reader:
+            if len(row[col]) > 0:
+                res.append(row[col])
+            
+    return res
+
 ####################################################################
 # MAIN
 ####################################################################
 
-C = np.zeros(n) # TODO: Fill in expected counts
-B = np.zeros(n) # TODO: Fill in observed counts
-node_names = [] # TODO: Fill in with names of each node
-resp_path = "path/to/folder/scores_resp.tsv" # TODO: Fill in path to folder
+# TODO: Fill in paths
+expected_path = '' 
+observed_path = ''
+names_path = ''
+resp_path = "path/to/folder/scores_resp.tsv"
 
+C = tsv_to_np(expected_path) 
+B = tsv_to_np(observed_path) 
+node_names = tsv_to_array(names_path)
 
 qin_est,qout_est,alpha_est,resps_est = em(C,B)
 resps_shift = shift_resps(resps_est, alpha_est)
